a85d6336fe5aafda782120b79178c96f388ded88
Mauricio González <mauricioge@gmail.com>
2024-01-17 23:23:38 -0600

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><style> spam{color:navy;} </style><body><h1><spam class=navy>TIGO!</spam></h1><h2>El nombre de esta VM es: <spam class=navy>$(hostname)</spam>.</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><style> spam{color:blue;} </style><body><h1><spam>TIGO!</spam></h1><h2>El nombre de esta VM es: <spam>$(hostname)</spam>.</h2></body></html>" | sudo tee -a /var/www/html/index.html
